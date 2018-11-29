13f1ae575d894df8b86aab71d983f40e2df074cb
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2018-11-29 07:35:03 -0800

    Camera2DProvided is activated now by a component to have an ability to have mixed cameras in the project (2d and 3d)

--- secsy-gaming/src/main/java/com/gempukku/secsy/gaming/camera2d/Camera2DProvider.java
+++ secsy-gaming/src/main/java/com/gempukku/secsy/gaming/camera2d/Camera2DProvider.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.gempukku.secsy.context.annotation.RegisterSystem;
 import com.gempukku.secsy.entity.EntityRef;
 import com.gempukku.secsy.entity.dispatch.ReceiveEvent;
+import com.gempukku.secsy.gaming.camera2d.component.Camera2DComponent;
 import com.gempukku.secsy.gaming.rendering.pipeline.GetCamera;
 
 import static com.gempukku.secsy.gaming.camera2d.Camera2DProvider.CAMERA_2D_PROFILE;
@@ -18,7 +19,7 @@ public class Camera2DProvider {
     private Camera camera = new OrthographicCamera();
 
     @ReceiveEvent
-    public void getCamera(GetCamera getCamera, EntityRef cameraEntity) {
+    public void getCamera(GetCamera getCamera, EntityRef cameraEntity, Camera2DComponent camera2D) {
         Adjust2dCamera cameraLocation = new Adjust2dCamera(
                 camera.viewportWidth, camera.viewportHeight,
                 getCamera.getWidth(), getCamera.getHeight(),
