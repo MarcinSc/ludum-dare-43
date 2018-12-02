365dccb110c6dab53a749dadc6683054a7ca0e43
MarcinSc <marcins78@gmail.com>
2018-12-03 06:39:26 +0700

    Player dies if there is too many bunnies.

--- core/src/main/java/com/gempukku/ld43/logic/damage/DeathSystem.java
+++ core/src/main/java/com/gempukku/ld43/logic/damage/DeathSystem.java
@@ -8,18 +8,33 @@ import com.gempukku.ld43.model.PlayerComponent;
 import com.gempukku.ld43.render.RenderText;
 import com.gempukku.secsy.context.annotation.Inject;
 import com.gempukku.secsy.context.annotation.RegisterSystem;
+import com.gempukku.secsy.context.system.AbstractLifeCycleSystem;
 import com.gempukku.secsy.entity.EntityManager;
 import com.gempukku.secsy.entity.EntityRef;
 import com.gempukku.secsy.entity.dispatch.ReceiveEvent;
+import com.gempukku.secsy.entity.game.GameLoopUpdate;
+import com.gempukku.secsy.entity.index.EntityIndex;
+import com.gempukku.secsy.entity.index.EntityIndexManager;
 import com.gempukku.secsy.gaming.component.Position2DComponent;
 import com.gempukku.secsy.gaming.time.TimeEntityProvider;
 
+import java.util.Iterator;
+
 @RegisterSystem
-public class DeathSystem {
+public class DeathSystem extends AbstractLifeCycleSystem {
     @Inject
     private TimeEntityProvider timeEntityProvider;
     @Inject
     private EntityManager entityManager;
+    @Inject
+    private EntityIndexManager entityIndexManager;
+
+    private EntityIndex dustBunnies;
+
+    @Override
+    public void initialize() {
+        dustBunnies = entityIndexManager.addIndexOnComponents(DustBunnyComponent.class);
+    }
 
     @ReceiveEvent
     public void playerDamaged(EntityDamaged entityDamaged, EntityRef entity, PlayerComponent player) {
@@ -34,6 +49,23 @@ public class DeathSystem {
     @ReceiveEvent
     public void dustBunnyOutOfBounds(EntityOutOfBounds entityOutOfBounds, EntityRef entity, DustBunnyComponent dustBunny) {
         entityManager.destroyEntity(entity);
+    }
+
+    @ReceiveEvent
+    public void checkForTooManyBunnies(GameLoopUpdate gameLoopUpdate) {
+        int count = 0;
+        for (EntityRef dustBunny : dustBunnies) {
+            count++;
+        }
+
+        if (count > 50) {
+            Iterator<EntityRef> iterator = entityManager.getEntitiesWithComponents(PlayerComponent.class).iterator();
+            if (iterator.hasNext()) {
+                EntityRef player = iterator.next();
+                player.send(new RenderText("The house has too much dust, try again.", 5000));
+                player.send(new PlayerDied());
+            }
+        }
     }
 
     @ReceiveEvent
