7c7309f87781fd06538bbc2f9db3a84dcc8e069a
MarcinSc <marcins78@gmail.com>
2018-12-02 23:50:27 +0700

    Splitting game entities between splash, menu and actual game

---
+++ core/src/main/java/com/gempukku/ld43/menu/GameExitSystem.java
@@ -0,0 +1,8 @@
+package com.gempukku.ld43.menu;
+
+import com.gempukku.secsy.context.annotation.RegisterSystem;
+
+@RegisterSystem
+public class GameExitSystem {
+
+}
--- core/src/main/java/com/gempukku/ld43/provider/GameEntityProviderImpl.java
+++ core/src/main/java/com/gempukku/ld43/provider/GameEntityProviderImpl.java
@@ -1,10 +1,13 @@
 package com.gempukku.ld43.provider;
 
+import com.gempukku.ld43.menu.GoToGame;
+import com.gempukku.ld43.menu.GoToMenu;
 import com.gempukku.secsy.context.annotation.Inject;
 import com.gempukku.secsy.context.annotation.RegisterSystem;
 import com.gempukku.secsy.context.system.AbstractLifeCycleSystem;
 import com.gempukku.secsy.entity.EntityManager;
 import com.gempukku.secsy.entity.EntityRef;
+import com.gempukku.secsy.entity.dispatch.ReceiveEvent;
 import com.gempukku.secsy.entity.game.GameEntityProvider;
 
 @RegisterSystem(shared = GameEntityProvider.class)
@@ -12,15 +15,34 @@ public class GameEntityProviderImpl extends AbstractLifeCycleSystem implements G
     @Inject
     private EntityManager entityManager;
 
-    private EntityRef gameLoopEntity;
+    private EntityRef splashGameLoopEntity;
+    private EntityRef menuGameLoopEntity;
+    private EntityRef gameGameLoopEntity;
+
+    private EntityRef currentGameLoopEntity;
 
     @Override
     public void initialize() {
-        gameLoopEntity = entityManager.createEntityFromPrefab("gameEntity");
+        splashGameLoopEntity = entityManager.createEntityFromPrefab("splashGameEntity");
+        menuGameLoopEntity = entityManager.createEntityFromPrefab("menuGameEntity");
+        gameGameLoopEntity = entityManager.createEntityFromPrefab("gameGameEntity");
+
+        currentGameLoopEntity = splashGameLoopEntity;
+    }
+
+
+    @ReceiveEvent
+    public void switchToMainMenu(GoToMenu goToMenu) {
+        currentGameLoopEntity = menuGameLoopEntity;
+    }
+
+    @ReceiveEvent
+    public void switchToGame(GoToGame goToGame) {
+        currentGameLoopEntity = gameGameLoopEntity;
     }
 
     @Override
     public EntityRef getGameEntity() {
-        return gameLoopEntity;
+        return currentGameLoopEntity;
     }
 }
