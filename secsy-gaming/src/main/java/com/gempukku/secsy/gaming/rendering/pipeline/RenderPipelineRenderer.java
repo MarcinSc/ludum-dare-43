fb6a5212e038a459eae1e02f114783b6fa79a32b
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2018-12-03 22:57:31 -0800

    Got rid of random bloat

--- secsy-gaming/src/main/java/com/gempukku/secsy/gaming/rendering/pipeline/RenderPipelineRenderer.java
+++ secsy-gaming/src/main/java/com/gempukku/secsy/gaming/rendering/pipeline/RenderPipelineRenderer.java
@@ -2,21 +2,16 @@ package com.gempukku.secsy.gaming.rendering.pipeline;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.VertexAttributes;
-import com.badlogic.gdx.graphics.g3d.Material;
-import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.graphics.g3d.ModelBatch;
-import com.badlogic.gdx.graphics.g3d.ModelInstance;
-import com.badlogic.gdx.graphics.g3d.attributes.DepthTestAttribute;
-import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.IndexBufferObject;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.graphics.glutils.VertexBufferObject;
 import com.gempukku.secsy.context.annotation.Inject;
 import com.gempukku.secsy.context.annotation.RegisterSystem;
 import com.gempukku.secsy.context.system.AbstractLifeCycleSystem;
 import com.gempukku.secsy.entity.EntityRef;
 import com.gempukku.secsy.gaming.rendering.RenderingSystem;
-import com.gempukku.secsy.gaming.rendering.copy.CopyShaderProvider;
 import com.gempukku.secsy.gaming.time.TimeManager;
 
 @RegisterSystem(profiles = "pipelineRenderer", shared = RenderingSystem.class)
@@ -28,27 +23,29 @@ public class RenderPipelineRenderer extends AbstractLifeCycleSystem implements R
     @Inject
     private TimeManager timeManager;
 
-    private CopyShaderProvider copyShaderProvider = new CopyShaderProvider();
-    private ModelBatch copyModelBatch;
-    private Model copyModel;
-    private ModelInstance copyModelInstance;
+    private ShaderProgram shaderProgram;
+    private VertexBufferObject vertexBufferObject;
+    private IndexBufferObject indexBufferObject;
 
     @Override
-    public void preInitialize() {
-        copyModelBatch = new ModelBatch(copyShaderProvider);
-        ModelBuilder modelBuilder = new ModelBuilder();
-        modelBuilder.begin();
-        MeshPartBuilder backgroundBuilder = modelBuilder.part("screen", GL20.GL_TRIANGLES, VertexAttributes.Usage.Position,
-                new Material(new DepthTestAttribute(0, false)));
-        backgroundBuilder.rect(
-                0, 1, 1,
-                0, 0, 1,
-                1, 0, 1,
-                1, 1, 1,
-                0, 0, 1);
-        copyModel = modelBuilder.end();
+    public void initialize() {
+        shaderProgram = new ShaderProgram(
+                Gdx.files.internal("shader/viewToScreenCoords.vert"),
+                Gdx.files.internal("shader/copy.frag"));
+        if (!shaderProgram.isCompiled())
+            throw new IllegalArgumentException("Error compiling shader: " + shaderProgram.getLog());
 
-        copyModelInstance = new ModelInstance(copyModel);
+        float[] verticeData = new float[]{
+                0, 0, 0,
+                0, 1, 0,
+                1, 0, 0,
+                1, 1, 0};
+        short[] indices = {0, 1, 2, 2, 1, 3};
+
+        vertexBufferObject = new VertexBufferObject(true, 4, VertexAttribute.Position());
+        indexBufferObject = new IndexBufferObject(true, indices.length);
+        vertexBufferObject.setVertices(verticeData, 0, verticeData.length);
+        indexBufferObject.setIndices(indices, 0, indices.length);
     }
 
     @Override
@@ -83,25 +80,34 @@ public class RenderPipelineRenderer extends AbstractLifeCycleSystem implements R
     }
 
     @Override
-    public void postDestroy() {
-        copyModel.dispose();
-        copyModelBatch.dispose();
+    public void destroy() {
+        vertexBufferObject.dispose();
+        indexBufferObject.dispose();
+        shaderProgram.dispose();
         renderPipeline.cleanup();
     }
 
     private void renderToScreen() {
-        copyShaderProvider.setSourceTextureIndex(0);
+        shaderProgram.begin();
+
+        cleanBuffer();
+
+        vertexBufferObject.bind(shaderProgram);
+        indexBufferObject.bind();
 
         Gdx.gl.glActiveTexture(GL20.GL_TEXTURE0);
         Gdx.gl.glBindTexture(GL20.GL_TEXTURE_2D, renderPipeline.getCurrentBuffer().getColorBufferTexture().getTextureObjectHandle());
 
-        cleanBuffer();
+        shaderProgram.setUniformf("u_sourceTexture", 0);
+        shaderProgram.setUniformf("u_textureStart", 0, 0);
+        shaderProgram.setUniformf("u_textureSize", 1, 1);
 
-        copyModelBatch.begin(null);
-        copyModelBatch.render(copyModelInstance);
-        copyModelBatch.end();
+        Gdx.gl20.glDrawElements(Gdx.gl20.GL_TRIANGLES, indexBufferObject.getNumIndices(), GL20.GL_UNSIGNED_SHORT, 0);
+        vertexBufferObject.unbind(shaderProgram);
+        indexBufferObject.unbind();
+
+        shaderProgram.end();
     }
-
 
     private void cleanBuffer() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
