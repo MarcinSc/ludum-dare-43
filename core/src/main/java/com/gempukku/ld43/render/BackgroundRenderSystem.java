e95246d275f9227eb84bc9e53218b4700bbfd740
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2018-12-03 17:41:43 -0800

    Doing tiling background with just one draw call

--- core/src/main/java/com/gempukku/ld43/render/BackgroundRenderSystem.java
+++ core/src/main/java/com/gempukku/ld43/render/BackgroundRenderSystem.java
@@ -1,24 +1,25 @@
 package com.gempukku.ld43.render;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.gempukku.ld43.model.GameScreenComponent;
-import com.gempukku.secsy.context.annotation.Inject;
 import com.gempukku.secsy.context.annotation.RegisterSystem;
 import com.gempukku.secsy.context.system.AbstractLifeCycleSystem;
 import com.gempukku.secsy.entity.EntityRef;
 import com.gempukku.secsy.entity.dispatch.ReceiveEvent;
-import com.gempukku.secsy.gaming.asset.texture.TextureAtlasProvider;
 import com.gempukku.secsy.gaming.rendering.pipeline.RenderToPipeline;
 
 @RegisterSystem
 public class BackgroundRenderSystem extends AbstractLifeCycleSystem {
-    @Inject
-    private TextureAtlasProvider textureAtlasProvider;
     private SpriteBatch spriteBatch;
+    private Texture wallpaper;
 
     @Override
     public void initialize() {
+        wallpaper = new Texture(Gdx.files.internal("images/wallpaper.png"));
+        wallpaper.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
         spriteBatch = new SpriteBatch();
     }
 
@@ -27,21 +28,19 @@ public class BackgroundRenderSystem extends AbstractLifeCycleSystem {
         int width = renderToPipeline.getWidth();
         int height = renderToPipeline.getHeight();
 
-        TextureRegion wallpaper = textureAtlasProvider.getTexture("sprites", "images/wallpaper.png");
+        TextureRegion region = new TextureRegion(wallpaper, 0, 0,
+                1f * width / wallpaper.getWidth(), 1f * height / wallpaper.getHeight());
 
         renderToPipeline.getRenderPipeline().getCurrentBuffer().begin();
         spriteBatch.begin();
-        for (int x = 0; x < width; x += wallpaper.getRegionWidth()) {
-            for (int y = 0; y < height; y += wallpaper.getRegionHeight()) {
-                spriteBatch.draw(wallpaper, x, y, wallpaper.getRegionWidth(), wallpaper.getRegionHeight());
-            }
-        }
+        spriteBatch.draw(region, 0, 0, width, height);
         spriteBatch.end();
         renderToPipeline.getRenderPipeline().getCurrentBuffer().end();
     }
 
     @Override
     public void destroy() {
+        wallpaper.dispose();
         spriteBatch.dispose();
     }
 }
